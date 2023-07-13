afb9544f66eb324070445cfbc03eed871af19c84
rauakmpct <rkloveak0118@gmail.com>
2023-07-13 22:31:53 +0530

    sfdfvb

---
+++ get.js
@@ -0,0 +1,12 @@
+// alert("Hello")
+
+
+let data=document.getElementById("a").innerText;
+// let data=document.getElementById("a").innerHTML;
+// data=document.getElementById("aa").innerHTML;
+// let data=document.getElementById("a").getAttribute("style");
+//let data=document.getElementById("a").getAttributeNode("style");
+// let data=document.getElementById("a").attributes[0];
+
+
+console.log(data);
